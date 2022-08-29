4d0ccc724857d63624bf8805fbce0ab46f22da48
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-08-29 18:04:59 +0530

    feat: added user controller

---
+++ lib/controllers/user_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
+import 'package:rootllyai/models/user.dart';
+
+class UserController extends GetxController {
+  static var tempSessionsDetails = {
+    "20-11-2020": {"10:20 am": 1, "11:21 am": 2},
+    "21-11-2020": {"10:20 am": 1, "11:21 am": 2},
+  };
+  var user = User(
+          name: "Jane",
+          totalSessions: 16,
+          totalTime: 16,
+          sessionsDetails: tempSessionsDetails)
+      .obs;
+
+  void updateSessions(var date, var time, int sessionId) {
+    user.update((user) {
+      user?.sessionsDetails[date][time]=sessionId;
+    });
+  }
+}

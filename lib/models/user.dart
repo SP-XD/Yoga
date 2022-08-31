5312b2a1ca37cbf641ff5945026ec9edf76d6ca2
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-08-31 23:10:45 +0530

    feat: implemented json convert to and from user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,16 +1,42 @@
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
 class User {
-  final String name;
+  User({
+    required this.name,
+    required this.sessionsCompletedToday,
+    required this.sessionsTargetToday,
+    required this.totalSessionsCompleted,
+    required this.totalTimeCompleted,
+    required this.sessionsDetails,
+  });
+
+  String name;
   int sessionsCompletedToday;
   int sessionsTargetToday;
   int totalSessionsCompleted;
   int totalTimeCompleted;
   Map sessionsDetails;
 
-  User(
-      {required this.name,
-      required this.sessionsCompletedToday,
-      required this.sessionsTargetToday,
-      required this.totalSessionsCompleted,
-      required this.totalTimeCompleted,
-      required this.sessionsDetails});
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        name: json["name"],
+        sessionsCompletedToday: json["sessionsCompletedToday"],
+        sessionsTargetToday: json["sessionsTargetToday"],
+        totalSessionsCompleted: json["totalSessionsCompleted"],
+        totalTimeCompleted: json["totalTimeCompleted"],
+        sessionsDetails: {},
+        // sessionDetails: sessionDetails.fromJson(json["sessionDetails"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "sessionsCompletedToday": sessionsCompletedToday,
+        "sessionsTargetToday": sessionsTargetToday,
+        "totalSessionsCompleted": totalSessionsCompleted,
+        "totalTimeCompleted": totalTimeCompleted,
+        // "sessionDetails": sessionDetails.toJson(),
+      };
 }

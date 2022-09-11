75a4b587162dfa15b231b2d05d5bc7bf7389ea06
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-09-11 20:44:31 +0530

    feat: implement initial firebase database call

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rootllyai/controllers/user_controller.dart';
 import 'package:rootllyai/firebase_options.dart';
 import 'package:rootllyai/main_layout.dart';
+import 'package:rootllyai/models/user.dart';
 import 'package:rootllyai/pages/home_page.dart';
 import 'package:rootllyai/pages/rehab_page.dart';
 
@@ -17,29 +20,46 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(MyApp());
   SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(systemNavigationBarColor: Colors.grey.shade200));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
+  final UserController userController = Get.put(UserController());
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(),
-      ),
-      getPages: [
-        GetPage(name: '/', page: () => MyApp()),
-        GetPage(name: '/home', page: () => HomePage()),
-        GetPage(name: '/rehab', page: () => RehabPage())
-      ],
-      debugShowCheckedModeBanner: false,
-      home: const MainLayout(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+        ),
+        getPages: [
+          GetPage(name: '/', page: () => MyApp()),
+          GetPage(name: '/home', page: () => HomePage()),
+          GetPage(name: '/rehab', page: () => RehabPage())
+        ],
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder(
+            future: FirebaseDatabase.instance.ref().child('users/1').once(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                var event = snapshot.data as DatabaseEvent;
+                debugPrint(
+                    'snapshot data future builder : ${event.snapshot.value}');
+                userController
+                    .updateUser(userFromJson(event.snapshot.value.toString()));
+                debugPrint(
+                    'user data after first initialization : ${userController.user.value}');
+                return const MainLayout();
+              }
+
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }));
   }
 }

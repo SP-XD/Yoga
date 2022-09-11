f76b08c12a4f2858805316ece05b7a00161c6016
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-09-11 20:43:16 +0530

    feat(FirebaseMethods): implement func fetchRecords

---
+++ lib/resources/firebase_methods.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:get/get.dart';
+import 'package:rootllyai/controllers/user_controller.dart';
+import 'package:rootllyai/models/user.dart';
+
+class FirebaseMethods {
+  final FirebaseDatabase _database = FirebaseDatabase.instance;
+  UserController userController = Get.put(UserController());
+
+  fetchRecords(int userId) async {
+    var records = FirebaseDatabase.instance.ref();
+
+    DatabaseReference snapshotRef = records.child("users/$userId");
+
+    snapshotRef.onValue.listen((DatabaseEvent event) {
+      if (event.snapshot.exists) {
+        print('firebase snapshot : ${event.snapshot.value}');
+        //updating user
+        userController.updateUser(userFromJson(event.snapshot.value.toString()));
+        print('user data to json: ${userController.user.value.sessionsDetails}');
+      } else {
+        print('No data available');
+      }
+    });
+  }
+}

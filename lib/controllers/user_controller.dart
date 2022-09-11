d8f1bb0f3524ff2832581be8603fa8461515a4ed
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-09-11 20:45:23 +0530

    feat: implement update user func

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -3,36 +3,19 @@ import 'package:get/get.dart';
 import 'package:rootllyai/models/user.dart';
 
 class UserController extends GetxController {
-  // testing data
-  static var tempSessionsDetails = {
-    "20-11-2020": {
-      "11:26 am": 2,
-      "11:27 am": 2,
-      "11:28 am": 2,
-      "11:29 am": 2,
-      "11:30 am": 2,
-      "11:40 am": 2,
-      "11:41 am": 2,
-      "11:42 am": 2,
-      "11:43 am": 2,
-      "11:44 am": 2,
-      "11:45 am": 2,
-      "11:46 am": 2,
-      "11:48 am": 2,
-      "11:59 am": 2,
-      "12:11 am": 2,
-      "12:21 am": 2,
-    },
-    "21-11-2020": {"1:22: pm": 3},
-  };
+  //initial data
   var user = User(
-          name: "Jane",
-          sessionsCompletedToday: 0,
-          sessionsTargetToday: 12,
-          totalSessionsCompleted: 16,
-          totalTimeCompleted: 16,
-          sessionsDetails: tempSessionsDetails)
-      .obs;
+      name: "Jane",
+      sessionsCompletedToday: 0,
+      //* hardcoded
+      sessionsTargetToday: 12,
+      totalSessionsCompleted: 0,
+      totalTimeCompleted: 0,
+      sessionsDetails: {}).obs;
+
+  updateUser(User user) {
+    this.user.value = user;
+  }
 
   void updateSessions(var date, var time, int sessionId) {
     user.update((user) {
